008139c36d48807eb90b424fa592b039836c91c0
Lolikarbuzik <lubiearbuzy5@gmail.com>
2025-01-14 20:36:08 +0100

    Logging information about the project

--- src/client/controllers/zircon.ts
+++ src/client/controllers/zircon.ts
@@ -1,6 +1,7 @@
 import { Controller, OnInit } from "@flamework/core";
 import Log, { Logger } from "@rbxts/log";
 import Zircon, { ZirconClient } from "@rbxts/zircon";
+import { $compileTime, $git, $package } from "rbxts-transform-debug";
 
 @Controller()
 export default class ZirconController implements OnInit {
@@ -12,5 +13,11 @@ export default class ZirconController implements OnInit {
 		ZirconClient.Init({
 			Keys: [Enum.KeyCode.F2],
 		});
+		Log.Info(
+			"Version {Version} build: {BuildDate} latest commit: {Commit}",
+			$package.version,
+			$compileTime("ISO-8601"),
+			$git().Commit,
+		);
 	}
 }
--- src/server/services/zircon.ts
+++ src/server/services/zircon.ts
@@ -1,7 +1,12 @@
 import { OnInit, Service } from "@flamework/core";
 import Log, { Logger } from "@rbxts/log";
-import Zircon, { ZirconConfigurationBuilder, ZirconServer } from "@rbxts/zircon";
-import { $compileTime, $package } from "rbxts-transform-debug";
+import Zircon, {
+	ZirconConfigurationBuilder,
+	ZirconDefaultGroup,
+	ZirconFunctionBuilder,
+	ZirconServer,
+} from "@rbxts/zircon";
+import { $compileTime, $git, $package } from "rbxts-transform-debug";
 
 @Service()
 export default class ZirconService implements OnInit {
@@ -13,6 +18,29 @@ export default class ZirconService implements OnInit {
 				.WriteTo(Zircon.Log.Console())
 				.Create(),
 		);
-		ZirconServer.Registry.Init(new ZirconConfigurationBuilder().Build());
+		ZirconServer.Registry.Init(
+			new ZirconConfigurationBuilder()
+				.CreateDefaultAdminGroup()
+				.CreateDefaultCreatorGroup()
+				.CreateDefaultUserGroup()
+				.AddFunction(
+					new ZirconFunctionBuilder("log_test").AddArgument("unknown").Bind((_, arg) => {
+						Log.Info("{}", arg);
+						Log.Warn("{}", arg);
+						// note: THIS DOESNT WORK DONT USE THIS
+						Log.Debug("{}", arg);
+						Log.Error("{}", arg);
+						Log.Fatal("{}", arg);
+					}),
+					[ZirconDefaultGroup.User],
+				)
+				.Build(),
+		);
+		Log.Info(
+			"Version {Version} build: {BuildDate} latest commit: {Commit}",
+			$package.version,
+			$compileTime("ISO-8601"),
+			$git().Commit,
+		);
 	}
 }
